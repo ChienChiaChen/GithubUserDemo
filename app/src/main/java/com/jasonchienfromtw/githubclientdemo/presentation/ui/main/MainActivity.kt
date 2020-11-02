26d3c261848ae2f16e91bb777685d667f1b4c90a
JasonChien <jasonchien@kkstream.com>
2020-11-02 10:14:27 +0800

    Git init.

---
+++ app/src/main/java/com/jasonchienfromtw/githubclientdemo/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jasonchienfromtw.githubclientdemo.presentation.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jasonchienfromtw.githubclientdemo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
